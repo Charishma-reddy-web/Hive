19ccae7f7b38cc353a30ab51d6b67c5d08beaf8e
Charishma Reddy <charishma.reddy@infoservices.com>
2026-06-15 18:27:55 +0530

    Fix build error: remove unused @studio-freight/react-lenis import

--- src/components/SmoothScrollProvider.tsx
+++ src/components/SmoothScrollProvider.tsx
@@ -1,10 +1,8 @@
 /* eslint-disable */
 'use client';
 
-import { ReactLenis } from '@studio-freight/react-lenis';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useEffect, useRef } from 'react';
 
 gsap.registerPlugin(ScrollTrigger);
 
